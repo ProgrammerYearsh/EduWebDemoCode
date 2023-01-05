30327077bb8a8bbf903d78d954ca2b83bfa63224
unknown <programmer.yearsh@gmail.com>
2023-01-05 17:00:49 +0530

    first commit

---
+++ src/pages/Responsive.js
@@ -0,0 +1,232 @@
+import styles from "./Responsive.module.css";
+
+const Responsive = () => {
+  return (
+    <div className={styles.responsive}>
+      <div className={styles.frameParent}>
+        <a className={styles.eLearnWrapper}>
+          <div className={styles.eLearn}>Edu Web</div>
+        </a>
+        <div className={styles.loginParent}>
+          <div className={styles.login}>Login</div>
+          <div className={styles.seeDetailWrapper}>
+            <b className={styles.seeDetail}>register</b>
+          </div>
+          <button className={styles.androidSettings}>
+            <img className={styles.shapeIcon} alt="" src="../shape.svg" />
+          </button>
+        </div>
+      </div>
+      <div className={styles.textParent}>
+        <div className={styles.text}>
+          <div className={styles.loremIpsumDolorAmParent}>
+            <b className={styles.loremIpsumDolorAm}>Lorem ipsum dolor amet</b>
+            <div className={styles.rectangle} />
+          </div>
+          <div className={styles.namSollicitudNunc}>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad
+          </div>
+          <div className={styles.moreInfoParent}>
+            <b className={styles.moreInfo}>More Info</b>
+            <button className={styles.seeDetailContainer}>
+              <b className={styles.seeDetail1}>SEE DETAIL</b>
+            </button>
+          </div>
+        </div>
+        <img className={styles.icon} alt="" src="../3784896-1@2x.png" />
+      </div>
+      <div className={styles.paginationWrapper}>
+        <div className={styles.pagination}>
+          <div className={styles.item}>
+            <div className={styles.businessSolution}>Exellent Education</div>
+            <div
+              className={styles.interdumEtMalesuad}
+            >{`The standard Lorem Ipsum `}</div>
+          </div>
+          <img className={styles.lineIcon} alt="" src="../line.svg" />
+          <div className={styles.item1}>
+            <div className={styles.freeProjectQuote}>Online support</div>
+            <div
+              className={styles.interdumEtMalesuad1}
+            >{`The standard Lorem Ipsum `}</div>
+          </div>
+          <img className={styles.lineIcon} alt="" src="../line1.svg" />
+          <div className={styles.item2}>
+            <div className={styles.nullaLobortisNunc}>
+              Professional Teachers
+            </div>
+            <div className={styles.interdumEtMalesuad2}>
+              The standard Lorem Ipsum
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.parent}>
+        <img className={styles.icon1} alt="" src="../3580922-1@2x.png" />
+        <div className={styles.bannerBottom}>
+          <div className={styles.titleprimaryLeftWrapper}>
+            <div className={styles.titleprimaryLeft}>
+              <div className={styles.secondaryLeft}>
+                <div className={styles.rectangle1} />
+                <b className={styles.ourServices}>About Us</b>
+                <div className={styles.div}>{` `}</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.aliquamGravidaQuamWrapper}>
+            <div className={styles.aliquamGravidaQuam}>
+              <p
+                className={styles.uasiArchitectoBeatae}
+              >{`uasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur `}</p>
+              <p className={styles.uasiArchitectoBeatae}>&nbsp;</p>
+              <p className={styles.dipisciVelitSed}>
+                dipisci velit, sed quia non numquam eius modi tempora incidunt
+                ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim
+                ad
+              </p>
+            </div>
+          </div>
+          <div className={styles.buttonflatprimaryWrapper}>
+            <div className={styles.buttonflatprimary}>
+              <div className={styles.rectangle2} />
+              <b className={styles.seeDetail2}>Join now</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameGroup}>
+        <div className={styles.ourServicesParent}>
+          <b className={styles.johnDoe}>Testimonials</b>
+          <div
+            className={styles.utSedErosFinibus}
+          >{`The standard Lorem Ipsum passage, used since `}</div>
+        </div>
+        <div className={styles.frameContainer}>
+          <div className={styles.frameDiv}>
+            <div className={styles.groupWrapper}>
+              <img className={styles.frameChild} alt="" src="../group-2.svg" />
+            </div>
+            <div className={styles.frameParent1}>
+              <div className={styles.namSollicitudinDigWrapper}>
+                <div className={styles.namSollicitudinDig}>
+                  <p
+                    className={styles.uasiArchitectoBeatae}
+                  >{`"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo  inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. `}</p>
+                </div>
+              </div>
+              <div className={styles.johnDoeWrapper}>
+                <b className={styles.johnDoe}>Lorem Ipsum</b>
+              </div>
+              <div className={styles.ceoOfCompanycomWrapper}>
+                <div className={styles.johnDoe}>C0 of Edu.com</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameDiv}>
+            <div className={styles.groupWrapper}>
+              <img className={styles.frameChild} alt="" src="../group-21.svg" />
+            </div>
+            <div className={styles.frameParent1}>
+              <div className={styles.namSollicitudinDigWrapper}>
+                <div className={styles.aliquamGravidaQuam}>
+                  <p
+                    className={styles.uasiArchitectoBeatae}
+                  >{`"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. `}</p>
+                </div>
+              </div>
+              <div className={styles.johnDoeWrapper}>
+                <b className={styles.johnDoe}>Lorem Ipsum</b>
+              </div>
+              <div className={styles.ceoOfCompanycomWrapper}>
+                <div className={styles.johnDoe}>C0 of Edu.com</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.dParent}>
+        <img className={styles.dIcon} alt="" src="../2d.svg" />
+        <div className={styles.subscribe}>
+          <div className={styles.subscribeOurNewsleWrapper}>
+            <b className={styles.johnDoe}>Subscribe our newsletter</b>
+          </div>
+          <div className={styles.sedImperdietEnimLWrapper}>
+            <div className={styles.sedImperdietEnimL}>
+              consectetur, adipisci velit, sed quia non numquam eius modi
+              tempora incidunt ut labore et dolore magnam aliquam quaerat
+            </div>
+          </div>
+          <div className={styles.frameParent4}>
+            <div className={styles.whatIsYourNameWrapper}>
+              <div className={styles.whatIsYourName}>
+                Enter your email address
+              </div>
+            </div>
+            <div className={styles.seeDetailFrame}>
+              <b className={styles.seeDetail3}>SUBSCRIBE</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.companyDescParent}>
+        <div className={styles.companyDesc}>
+          <div className={styles.travelWrapper}>
+            <div className={styles.travel}>Travel</div>
+          </div>
+          <div className={styles.loremIpsumDolorSitAmetCoWrapper}>
+            <div className={styles.namSollicitudinDig}>
+              <p className={styles.uasiArchitectoBeatae}>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.navColumnsParent}>
+          <div className={styles.navColumns}>
+            <div className={styles.navRow}>
+              <b className={styles.company}>Company</b>
+              <div className={styles.about}>About</div>
+              <div className={styles.about}>Careers</div>
+              <div className={styles.about}>Mobile</div>
+            </div>
+            <div className={styles.navRow}>
+              <b className={styles.company}>Contact</b>
+              <div className={styles.about}>Help/FAQ</div>
+              <div className={styles.about}>Affilates</div>
+            </div>
+          </div>
+          <div className={styles.frameParent5}>
+            <div className={styles.groupFrame}>
+              <img className={styles.frameInner} alt="" src="../group-9.svg" />
+            </div>
+            <div className={styles.group9CopyWrapper}>
+              <img
+                className={styles.frameInner}
+                alt=""
+                src="../group-9-copy.svg"
+              />
+            </div>
+            <div className={styles.group9CopyWrapper}>
+              <img
+                className={styles.frameInner}
+                alt=""
+                src="../group-9-copy-2.svg"
+              />
+            </div>
+            <div className={styles.group9Copy3Wrapper}>
+              <img
+                className={styles.frameInner}
+                alt=""
+                src="../group-9-copy-3.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Responsive;
